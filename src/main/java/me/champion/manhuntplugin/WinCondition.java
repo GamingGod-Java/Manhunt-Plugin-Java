9f6281148afc311b8f3f27d3c018b63cb77c7686
Andre Binbow <andrebinbpw@gmail.com>
2024-01-15 19:10:39 +0000

    Added fireworks of blue/red colour on runner/hunter win

--- src/main/java/me/champion/manhuntplugin/WinCondition.java
+++ src/main/java/me/champion/manhuntplugin/WinCondition.java
@@ -1,6 +1,9 @@
 package me.champion.manhuntplugin;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.entity.Firework;
 import org.bukkit.World;
 import org.bukkit.entity.EnderDragon;
 import org.bukkit.entity.Player;
@@ -9,6 +12,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EnderDragonChangePhaseEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerPortalEvent;
 
@@ -32,6 +36,7 @@ public class WinCondition implements Listener {
                 RunnerWin = true;
                 for (Player player : Bukkit.getOnlinePlayers()) {
                     player.sendTitle("§bRunners win", "Game Over", 20, 40, 10);
+                    spawnFirework(player, Color.AQUA);
                 }
             }
         }
@@ -46,6 +51,7 @@ public class WinCondition implements Listener {
                     ZombieWin = true;
                     for (Player player : Bukkit.getOnlinePlayers()) {
                         player.sendTitle("§cZombies Win", "Game Over", 20, 40, 10);
+                        spawnFirework(player, Color.RED);
                     }
                 }
             }
@@ -67,5 +73,20 @@ public class WinCondition implements Listener {
         }
 
 
+    }
+
+    private void spawnFirework(Player player, Color color) {
+        Firework firework = player.getWorld().spawn(player.getLocation(), Firework.class);
+        FireworkMeta fireworkMeta = firework.getFireworkMeta();
+
+        FireworkEffect.Builder effect = FireworkEffect.builder();
+        effect.withColor(color);
+        effect.with(FireworkEffect.Type.BALL_LARGE);
+        effect.trail(true); // Add trail effect
+        effect.flicker(true); // Add twinkle effect
+
+        fireworkMeta.addEffect(effect.build());
+        fireworkMeta.setPower(1);
+        firework.setFireworkMeta(fireworkMeta);
     }
 }
